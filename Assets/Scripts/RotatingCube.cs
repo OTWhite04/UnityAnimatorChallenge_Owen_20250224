7e82b250897dc39e7e1aeb7394d57183d61f704c
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-24 15:29:42 -0400

    Working on rotation script

--- Assets/Scripts/RotatingCube.cs
+++ Assets/Scripts/RotatingCube.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class RotatingCube : MonoBehaviour
 {
+    
+    //Animator reference.
+    Animator m_Animator;
+    //Bool for the jump.
+    bool m_Spin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_Spin = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Tab) && m_Animator.GetBool("Spin") == false)
+        {
+            m_Animator.SetBool("Spin", true);
+        }
+        else 
+        {
+            m_Animator.SetBool("Spin", false);
+
+        } 
         
     }
 }
