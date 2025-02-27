eede6cb58cbae6ac93f4abb1daa699e4d78250b6
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-27 15:14:48 -0400

    adding comments

--- Assets/Scripts/AnimatorChallengeScript.cs
+++ Assets/Scripts/AnimatorChallengeScript.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 public class RotatingCube : MonoBehaviour
 {
     
-    //Animator reference.
+    //Animator references.
     public Animator cubeAnimator;
     public Animator triggerAnimator;
     public Animator floatAnimator;
-    //Bool for the jump.
-    bool m_Spin;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-                    
+                   
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If and else if statements for the rotating cube that start and stop once tab is pressed.
         if (Input.GetKeyDown(KeyCode.Tab) && cubeAnimator.GetBool("Rotate") == false)
         {
             cubeAnimator.SetBool("Rotate", true);
@@ -29,7 +27,6 @@ public class RotatingCube : MonoBehaviour
         else if(Input.GetKeyDown(KeyCode.Tab) && cubeAnimator.GetBool("Rotate") == true)
         {
             cubeAnimator.SetBool("Rotate", false);
-
         } 
         
     }
