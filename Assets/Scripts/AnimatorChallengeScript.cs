71f364c3783a632262fb42ea15fe46b9a8d6bcef
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-27 15:12:56 -0400

    Finished Cube Rotation

--- Assets/Scripts/AnimatorChallengeScript.cs
+++ Assets/Scripts/AnimatorChallengeScript.cs
@@ -2,31 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimatorChallengeScript : MonoBehaviour
+public class RotatingCube : MonoBehaviour
 {
     
     //Animator reference.
-    Animator m_Animator;
+    public Animator cubeAnimator;
+    public Animator triggerAnimator;
+    public Animator floatAnimator;
     //Bool for the jump.
     bool m_Spin;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Animator = gameObject.GetComponent<Animator>();
-        m_Spin = false;
+        
+                    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab) && m_Animator.GetBool("Spin") == false)
+        if (Input.GetKeyDown(KeyCode.Tab) && cubeAnimator.GetBool("Rotate") == false)
         {
-            m_Animator.SetBool("Cube_AnimController", true);
+            cubeAnimator.SetBool("Rotate", true);
         }
-        else 
+        else if(Input.GetKeyDown(KeyCode.Tab) && cubeAnimator.GetBool("Rotate") == true)
         {
-            m_Animator.SetBool("Cube_AnimController", false);
+            cubeAnimator.SetBool("Rotate", false);
 
         } 
         
